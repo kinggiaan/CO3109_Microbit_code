da0da16f87a11026feddaf4f9fcb930fab61815f
kinggiaan <56713472+kinggiaan@users.noreply.github.com>
2022-04-27 07:08:46 +0700

    Demo_Fin

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def on_data_received():
     global cmd
     cmd = serial.read_until(serial.delimiters(Delimiters.HASH))
@@ -8,15 +7,28 @@ def on_data_received():
         NPNLCD.show_string("MSTR:", 9, 1)
         NPNLCD.show_number(99, 14, 1)
     elif cmd == "BUTTON":
-        NPNLCD.show_string("BUTTON", 0, 0)
-        NPNLCD.show_string("Value = 1", 7, 0)
-        basic.show_number(1)
-    elif cmd == "POWER":
-        NPNLCD.clear()
+        NPNLCD.show_string("RELEASING PROD", 0, 0)
+    elif cmd == "RELAY":
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        basic.pause(1000)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+    elif cmd == "RESTART":
+        NPNLCD.show_string("TEMP:", 0, 1)
+        NPNLCD.show_number(NPNBitKit.dht11_temp(), 5, 1)
+        NPNLCD.show_string("MSTR:", 9, 1)
+        NPNLCD.show_number(NPNBitKit.dht11_hum(), 14, 1)
+    elif cmd == "OUT_OF_PROD":
+        NPNLCD.show_string("\"NOT ENOUGH PROD", 0, 0)
+        basic.show_icon(IconNames.SAD)
+        basic.pause(500)
+        NPNLCD.show_string("\"SORRY!", 0, 1)
+    else:
+        NPNLCD.show_string("PRODUCT AT:", 0, 0)
+        NPNLCD.show_string(cmd, 12, 0)
 serial.on_data_received(serial.delimiters(Delimiters.HASH), on_data_received)
 
 def DHT_REPORT():
-    NPNBitKit.dht11_read(DigitalPin.P0)
+    NPNBitKit.dht11_read(DigitalPin.P2)
     NPNLCD.show_string("TEMP:", 0, 1)
     NPNLCD.show_number(NPNBitKit.dht11_temp(), 5, 1)
     NPNLCD.show_string("MSTR:", 9, 1)
@@ -25,13 +37,22 @@ cmd = ""
 NPNLCD.lcd_init()
 basic.pause(500)
 NPNLCD.backlight_on()
-
+NPNBitKit.dht11_read(DigitalPin.P2)
+count10s = 0
 def on_forever():
+    NPNBitKit.dht11_read(DigitalPin.P2)
     basic.show_icon(IconNames.SMALL_HEART)
-    NPNBitKit.dht11_read(DigitalPin.P0)
-    if pins.digital_read_pin(DigitalPin.P2) == 1:
+    basic.show_string("" + str((pins.digital_read_pin(DigitalPin.P0))))
+    # PULL DOWN
+    # 
+    if pins.digital_read_pin(DigitalPin.P0) == 0:
         basic.pause(500)
-        if pins.digital_read_pin(DigitalPin.P2) == 1:
-            serial.write_string("!POWER:" + "1" + "#")
-            pins.digital_write_pin(DigitalPin.P2, 1)
+        if pins.digital_read_pin(DigitalPin.P0) == 0:
+            serial.write_string("!BUTTON:" + "1" + "#")
+            pins.digital_write_pin(DigitalPin.P0, 1)
+    if input.button_is_pressed(Button.A):
+        basic.show_icon(IconNames.HEART)
+        serial.write_string("!BUTTON:" + "1" + "#")
+    if input.button_is_pressed(Button.B):
+        serial.write_string("!TEMP:" + str(NPNBitKit.dht11_temp()) + "#")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,32 +6,74 @@ serial.onDataReceived(serial.delimiters(Delimiters.Hash), function () {
         NPNLCD.ShowString("MSTR:", 9, 1)
         NPNLCD.ShowNumber(99, 14, 1)
     } else if (cmd == "BUTTON") {
-        NPNLCD.ShowString("BUTTON", 0, 0)
-        NPNLCD.ShowString("Value = 1", 7, 0)
-        basic.showNumber(1)
-    } else if (cmd == "POWER") {
+        NPNLCD.ShowString("RELEASING PROD", 0, 0)
+    } else if (cmd == "RESTART") {
+        NPNLCD.ShowString("TEMP:", 0, 1)
+        NPNLCD.ShowNumber(NPNBitKit.DHT11Temp(), 5, 1)
+        NPNLCD.ShowString("MSTR:", 9, 1)
+        NPNLCD.ShowNumber(NPNBitKit.DHT11Hum(), 14, 1)
+    } else if (cmd == "OUT_OF_PROD") {
+        NPNLCD.ShowString("\"NOT ENOUGH PROD", 0, 0)
+        basic.showIcon(IconNames.Sad)
+        basic.pause(500)
+        NPNLCD.ShowString("\"SORRY!", 0, 1)
+    } else if (cmd == "TIME_OUT_LCD") {
+        NPNLCD.ShowString("TIME OUT!!!", 0, 0)
+        NPNLCD.ShowString("NEXT ORDER", 0, 1)
+    } else if (cmd == "DONE_ORDER") {
+        NPNLCD.ShowString("THANK YOU !!!", 0, 1)
+        basic.showIcon(IconNames.Happy)
+        NPNLCD.ShowString("NEXT ORDER", 0, 1)
         NPNLCD.clear()
+    } else if (cmd == "RE_LAY") {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    } else if (cmd == "TAKEN_YET") {
+        COUNT_10s()
+    } else {
+        NPNLCD.ShowString("PRODUCT AT:", 0, 0)
+        NPNLCD.ShowString(cmd, 12, 0)
     }
 })
 function DHT_REPORT () {
-    NPNBitKit.DHT11Read(DigitalPin.P0)
+    NPNBitKit.DHT11Read(DigitalPin.P2)
     NPNLCD.ShowString("TEMP:", 0, 1)
     NPNLCD.ShowNumber(NPNBitKit.DHT11Temp(), 5, 1)
     NPNLCD.ShowString("MSTR:", 9, 1)
     NPNLCD.ShowNumber(NPNBitKit.DHT11Hum(), 14, 1)
 }
+function COUNT_10s () {
+    count10s = 0
+    if (count10s < 50) {
+        // SOUND SENSOR
+        // 
+        if (pins.digitalReadPin(DigitalPin.P0) >= 100) {
+            return
+        }
+        count10s += 1
+        basic.pause(200)
+    } else {
+        serial.writeString("!TIME_OUT" + "1" + "#")
+        return
+    }
+}
 let cmd = ""
+let count10s = 0
+count10s = 0
 NPNLCD.LcdInit()
 basic.pause(500)
 NPNLCD.BacklightOn()
+NPNBitKit.DHT11Read(DigitalPin.P2)
 basic.forever(function () {
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showString("" + (pins.digitalReadPin(DigitalPin.P0)))
     NPNBitKit.DHT11Read(DigitalPin.P2)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showString("" + pins.digitalReadPin(DigitalPin.P0))
+    // PULL DOWN
     if (pins.digitalReadPin(DigitalPin.P0) == 0) {
         basic.pause(500)
         if (pins.digitalReadPin(DigitalPin.P0) == 0) {
-            serial.writeString("!POWER:" + "1" + "#")
+            serial.writeString("!BUTTON:" + "1" + "#")
             pins.digitalWritePin(DigitalPin.P0, 1)
         }
     }
@@ -40,6 +82,6 @@ basic.forever(function () {
         serial.writeString("!BUTTON:" + "1" + "#")
     }
     if (input.buttonIsPressed(Button.B)) {
-        serial.writeString("!TEMP:" + NPNBitKit.DHT11Temp() + "#")
+        serial.writeString("!TEMP:" + ("" + NPNBitKit.DHT11Temp()) + "#")
     }
 })
